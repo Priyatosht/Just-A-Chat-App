ae29ec6f6fa120294996f505757d8f8553d431c9
unknown <sreetejreddy1998@gmail.com>
2021-12-11 22:41:17 +0530

    just a chat init

---
+++ src/main/java/com/chat/justachatapp/JustAChatAppApplication.java
@@ -0,0 +1,13 @@
+package com.chat.justachatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustAChatAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JustAChatAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chat/justachatapp/JustAChatAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chat.justachatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustAChatAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
