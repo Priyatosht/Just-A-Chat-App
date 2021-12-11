27bbce2b19fec001cc4a651eb97fd6cde2d49531
unknown <sreetejreddy1998@gmail.com>
2021-12-11 22:46:37 +0530

    feat: auth

--- src/main/java/com/chat/justachatapp/JustAChatAppApplication.java
+++ src/main/java/com/chat/justachatapp/JustAChatAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JustAChatAppApplication {
 
+// just a chat app
 	public static void main(String[] args) {
 		SpringApplication.run(JustAChatAppApplication.class, args);
 	}
